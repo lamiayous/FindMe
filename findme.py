663fadeeb94f9c5da6ce7277f08caaa7f0df4b28
lamiayous <124199862+lamiayous@users.noreply.github.com>
2023-08-25 22:41:45 +0100

    crop image code added objectdetector_code.py

--- findme.py
+++ findme.py
@@ -1,9 +1,8 @@
 import argparse 
-import cv2
-import os
 from ultralytics import YOLO
 from autoencoder_inference import encoder_code
 from objectdetector_code import ObjectDetector
+
 
 def main():
 
@@ -15,16 +14,20 @@ def main():
     parser.add_argument("--image", type=str, default=None)
     args = parser.parse_args() 
     img_file =args.image
-
     ###### Object Detector #######
     object_detector = ObjectDetector()
     out_object_detector = object_detector.run_inference(img_file)
-    result = out_object_detector[0]
-    
-    #extracting name of detected class
-    object_name = object_detector.name_of_class(result, model)
 
-    ###### Encoder #######
+    if len(out_object_detector) == 0:
+        print("no object detected")
+        exit()
+
+    #extracting name of detected class
+    result = out_object_detector[0]
+    object_name = object_detector.name_of_class(result, model)
+    object_detector.crop_image(object_name, img_file, result)
+    
+    # ###### Encoder #######
     encoder_code("last.pth")
 
 if __name__ == "__main__":
--- objectdetector_code.py
+++ objectdetector_code.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import os
+from PIL import Image
 
 class ObjectDetector:
     def __init__(self, weights="best.pt"):
@@ -16,7 +17,18 @@ class ObjectDetector:
             object_name = names[int(r.boxes.cls)]
         return object_name
     
-    def crop_image(self, object_name):
+    def crop_image(self, object_name, img_file, result):
         img_filepath = "./imgs/test/" + object_name
         if os.path.exists(img_filepath) == False:
             os.makedirs(img_filepath)
+        
+
+        #getting co-ordinates for bounding boxes
+        img = Image.open(img_file)
+        box = result.boxes[0]
+        cords = box.xyxy[0].tolist()
+        x1, y1, x2, y2 = cords
+        print("Coordinates:", x1)
+        img_res = img.crop((x1, y1, x2, y2)) 
+        img_res.show() 
+        img.show()
