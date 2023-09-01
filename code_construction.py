762a37f8c4e8ef57386e5af6b50c2c9ddbee6f37
lamiayous <124199862+lamiayous@users.noreply.github.com>
2023-09-01 22:14:45 +0100

    updates

--- code_construction.py
+++ code_construction.py
@@ -2,14 +2,13 @@ import torch
 from torchvision.utils import save_image
 
 device = torch.device('cpu')
-def test_image_reconstruction(net, testloader):
-        for batch in testloader:
-            img, _ = batch
-            img = img.to(device)
-            # img_code = encoder(img)
-            img_code, outputs = net(img)
-            outputs = outputs.view(outputs.size(0), 3, 128, 128).cpu().data
-            save_image(img, 'original_images.png')
-            save_image(outputs, 'reconstructed_images.png')
-            break
-        return img_code
+def image_code_extraction(net, testloader):
+    for batch in testloader:
+        img, _ = batch
+        img = img.to(device)
+        img_code, outputs = net(img)
+        outputs = outputs.view(outputs.size(0), 3, 128, 128).cpu().data
+        save_image(img, 'original_images.png')
+        save_image(outputs, 'reconstructed_images.png')
+        break
+    return img_code
