c1085529d971078ac444486b3729a0187c14dbd5
lamiayous <124199862+lamiayous@users.noreply.github.com>
2023-09-01 22:03:07 +0100

    improving readability

--- findme.py
+++ findme.py
@@ -20,8 +20,6 @@ def main():
         object_name = query[0]
         img_file =  query[1]
 
-    ###### Object Detector #######
-
     object_detector = ObjectDetector()
     out_object_detector = object_detector.run_inference(img_file) #runnning inference
     no_object_detected = len(out_object_detector.boxes) #how many objects are detected
@@ -30,20 +28,19 @@ def main():
         print("No object has been detected, try another image")
 
     else:
-        for object in range(no_object_detected):
-            result = out_object_detector[object] #result of detected object (dictionary)
-            if upload != None:
+        if upload != None:
+            for object in range(no_object_detected):
+                result = out_object_detector[object] #result of detected object (dictionary)
                 object_name = object_detector.name_of_class(result, model) #getting object name (only in upload mode)
-            object_detector.crop_image(object_name, img_file, result) #cropping detected image
-
-            ####### Encoder #######
-            unique_code = encoder_code("last.pth")
-
-            ###### Database ######
-            if upload != None:
+                object_detector.crop_image(object_name, img_file, result) #cropping detected image
+                unique_code = encoder_code("last.pth")
                 database_upload(object_name, unique_code) # (only in upload mode)
-            else:
-                database_query(unique_code)
+                
+        if query != None:
+            result = out_object_detector[object] #result of detected object (dictionary)
+            object_detector.crop_image(object_name, img_file, result) #cropping detected image and storing into file
+            unique_code = encoder_code("last.pth") 
+            database_query(unique_code)
 
 if __name__ == "__main__":
     main()
