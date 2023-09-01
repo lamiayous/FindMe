841e7322acab8cd1fab24071ed99914f503d4eb2
lamiayous <124199862+lamiayous@users.noreply.github.com>
2023-09-01 22:15:07 +0100

    Update autoencoder_inference.py

--- autoencoder_inference.py
+++ autoencoder_inference.py
@@ -1,40 +1,24 @@
 from encoder_decoder import encoder
 from encoder_decoder import decoder
 from encoder_decoder import autoencoder
-from code_construction import test_image_reconstruction
+from code_construction import image_code_extraction
 import torch
 import torchvision.transforms as transforms 
 import torchvision.datasets as Datasets
 import matplotlib.pyplot as plt
 import numpy as np
-# class encoder_code:
-def unique_code_files(img_codes):
-    print(img_codes)
-    codes_arr = torch.detach(img_codes).numpy()
-    print
-    print(codes_arr)
-    no_imgs = len(codes_arr)
-    index = 0 
-
-    while index < no_imgs:
-        img_no = str(index)
-        filename = ("ImagenumberTest1.txt")
-        np.savetxt(filename, codes_arr[index])
-        index += 1
 
 def encoder_code(trained_model):
-    #data loader
-        test_dataset_path = 'imgs/test/'
-        transform = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
-        test_dataset = Datasets.ImageFolder(root = test_dataset_path, transform = transform)
-        test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = 1, shuffle=True)
-        #load the model 
-        FILE =trained_model
+    test_dataset_path = 'imgs/test/'
+    transform = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()]) #setting transforms
+    test_dataset = Datasets.ImageFolder(root = test_dataset_path, transform = transform) #applying transforms to dataset
+    test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = 1, shuffle=True)
 
-        model = autoencoder(encoder(), decoder())
-        model.load_state_dict(torch.load(FILE))
-        model.eval()
-        img_codes = test_image_reconstruction(net = model, testloader = test_loader)
+    FILE =trained_model #load the model
 
-        unique_code_files(img_codes)
-        return img_codes
+    model = autoencoder(encoder(), decoder()) #calling autoencoder
+    model.load_state_dict(torch.load(FILE)) #loading pretrained model
+    model.eval() #this is to turn off all dropout layers
+    img_codes = image_code_extraction(net = model, testloader = test_loader) #extracting image codes
+
+    return img_codes
