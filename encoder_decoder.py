faed690bb2e030bac9ddbf74dba2ff3ee227b862
lamiayous <124199862+lamiayous@users.noreply.github.com>
2023-09-02 22:02:08 +0100

    clean up

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -4,31 +4,31 @@ import torch
 device = torch.device('cpu')
 
 class encoder(nn.Module):
-  def __init__(self, out_channels=16, latent_dim=256):
+  def __init__(self, out_channels=16, latent_dim=256, activation_fn = nn.ReLU()):
     super().__init__()
 
     self.net = nn.Sequential(
-        nn.Conv2d(3, 16, 3, padding=1), 
-        nn.ReLU(),
-        nn.Conv2d(16, 16, 3, padding=1), 
-        nn.ReLU(),
-        nn.Conv2d(16, 32, 3, padding=1,stride = 2),
-        nn.ReLU(),
-        nn.Conv2d(32, 32, 3, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(32, 64, stride = 2, kernel_size = 3, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(64, 64, 3, padding=1), 
-        nn.ReLU(),
-        nn.Conv2d(64, 128, 3, padding=1, stride =2), 
-        nn.ReLU(),
+        nn.Conv2d(3, out_channels, 3, padding=1), 
+        activation_fn,
+        nn.Conv2d(out_channels, out_channels, 3, padding=1), 
+        activation_fn,
+        nn.Conv2d(out_channels, out_channels*2, 3, padding=1,stride = 2),
+        activation_fn,
+        nn.Conv2d(out_channels*2, out_channels*2, 3, padding=1),
+        activation_fn,
+        nn.Conv2d(out_channels*2, out_channels*4, stride = 2, kernel_size = 3, padding=1),
+        activation_fn,
+        nn.Conv2d(out_channels*4, out_channels*4, 3, padding=1), 
+        activation_fn,
+        nn.Conv2d(out_channels*4, out_channels*8, 3, padding=1, stride =2), 
+        activation_fn,
         nn.Dropout(0.05),
-        nn.Conv2d(128, 128, 3, padding=1), 
-        nn.ReLU(),
+        nn.Conv2d(out_channels*8, out_channels*8, 3, padding=1), 
+        activation_fn,
         nn.Dropout(0.05),
         nn.Flatten(),
         nn.Linear(32*out_channels*8*8, latent_dim),
-        nn.ReLU(),
+        activation_fn,
     )
 
   def forward(self, x):
@@ -38,36 +38,36 @@ class encoder(nn.Module):
   
 
 class decoder(nn.Module):
-  def __init__(self, in_channels=3, out_channels=16, latent_dim=256):
+  def __init__(self, in_channels=3, out_channels=16, latent_dim=256, activation_fn = nn.ReLU()):
     super().__init__()
 
     self.out_channels = out_channels
 
     self.linear = nn.Sequential(
         nn.Linear(latent_dim, 32*out_channels*8*8),
-        nn.ReLU(),
+        activation_fn,
     )
 
     self.conv = nn.Sequential(
         nn.ConvTranspose2d(8*out_channels, 8*out_channels, 3, padding=1), 
-        nn.ReLU(),
+        activation_fn,
         nn.Dropout(0.05),
         nn.ConvTranspose2d(8*out_channels, 4*out_channels, 3, padding=1, 
                            stride=2, output_padding=1), 
-        nn.ReLU(),
+        activation_fn,
         nn.Dropout(0.05),
         nn.ConvTranspose2d(4*out_channels, 4*out_channels, 3, padding=1), 
-        nn.ReLU(),
+        activation_fn,
         nn.ConvTranspose2d(4*out_channels, 2*out_channels, 3, padding=1, 
                            stride=2, output_padding=1),
-        nn.ReLU(),
+        activation_fn,
         nn.ConvTranspose2d(2*out_channels, 2*out_channels, 3, padding=1),
-        nn.ReLU(),
+        activation_fn,
         nn.ConvTranspose2d(2*out_channels, out_channels, 3, padding=1, 
                            stride=2, output_padding=1),
-        nn.ReLU(),
+        activation_fn,
         nn.ConvTranspose2d(out_channels, out_channels, 3, padding=1),
-        nn.ReLU(),
+        activation_fn,
         nn.ConvTranspose2d(out_channels, in_channels, 3, padding=1)
     )
 
